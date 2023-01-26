761a34286eda3ad23ee773581ce84cf479d11334
Karlis <karlisstekels.dev@gmail.com>
2023-01-26 10:33:44 +0200

    Initial Commit

---
+++ FizzBuzz/FizzBuzzApp.swift
@@ -0,0 +1,17 @@
+//
+//  FizzBuzzApp.swift
+//  FizzBuzz
+//
+//  Created by karlis.stekels on 26/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct FizzBuzzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
